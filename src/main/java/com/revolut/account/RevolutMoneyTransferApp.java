dcc2a704891f647c9e11164b08303670b778e594
Sudharshan Rao <sudharshanrao@gmail.com>
2018-12-24 00:25:27 -0600

    added controller as singleton

--- src/main/java/com/revolut/account/RevolutMoneyTransferApp.java
+++ src/main/java/com/revolut/account/RevolutMoneyTransferApp.java
@@ -10,22 +10,15 @@ import org.slf4j.LoggerFactory;
 
 import com.revolut.account.controller.AccountController;
 
-import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
-
 public class RevolutMoneyTransferApp extends Application
 {
 	private static final Logger logger = LoggerFactory.getLogger(RevolutMoneyTransferApp.class);
-	public static void moneyTransferHandler(HttpServerExchange exchange) {
-	    exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, 
-	    		"text/plain");
-	    exchange.getResponseSender().send("Hello World!");
-	}
 	
 	@Override
-    public Set<Class<?>> getClasses() {
-        final Set<Class<?>> clazzes = new HashSet<Class<?>>();
-        clazzes.add(AccountController.class);
-        return clazzes;
+    public Set<Object> getSingletons() {
+        final Set<Object> set = new HashSet<>();
+        set.add(new AccountController());
+        return set;
     }
+
 }
--- src/main/java/com/revolut/account/controller/AccountController.java
+++ src/main/java/com/revolut/account/controller/AccountController.java
@@ -1,21 +1,17 @@
 package com.revolut.account.controller;
 
-import java.io.IOException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import javax.xml.ws.spi.http.HttpExchange;
-import javax.xml.ws.spi.http.HttpHandler;
 
 import com.revolut.account.dao.InMemoryDao;
 import com.revolut.account.dao.InMemoryDaoImpl;
  
 @Path("/account")
-public class AccountController extends HttpHandler
+public class AccountController
 {
 	InMemoryDao inMemoryDao = new InMemoryDaoImpl(); 
 	
@@ -33,11 +29,4 @@ public class AccountController extends HttpHandler
     	inMemoryDao.createCustomer(email);
         return Response.status(200).build();
     }
-
-	@Override
-	public void handle(HttpExchange exchange) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
-    
 }
