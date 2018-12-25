a507a093ab23c1a1f4e892575b797040c1d9c7c8
Sudharshan Rao <sudharshanrao@gmail.com>
2018-12-25 16:50:51 -0600

    removed service class.

--- src/main/java/com/revolut/account/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.revolut.account.service;
-
-import com.revolut.account.model.Account;
-import com.revolut.account.model.Customer;
-import com.revolut.account.model.Transfer;
-
-public class AccountService {
-	
-}
